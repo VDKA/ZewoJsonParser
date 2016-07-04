8ce84b5c9bd0552fd0d737f51541c56ba99368c4
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-07-04 16:58:28 +1000

    Initial Commit

---
+++ Sources/ZewoJsonParser.swift
@@ -0,0 +1,61 @@
+
+import C7
+import StructuredData
+import GenericJsonParser
+
+public struct JSONParser {
+
+  static func toObject(_ input: [(String, StructuredData)]) -> StructuredData {
+
+    var dict: [String: StructuredData] = [:]
+    for (key, value) in input {
+      dict[key] = value
+    }
+    return .dictionary(dict)
+  }
+
+  static func toArray(_ input: [StructuredData]) -> StructuredData {
+    return .array(input)
+  }
+
+  static func toNull() -> StructuredData {
+    return .null
+  }
+
+  static func toBool(_ input: Bool) -> StructuredData {
+    return .bool(input)
+  }
+
+  static func toString(_ input: String) -> StructuredData {
+
+    return .string(input)
+  }
+
+  static func toNumber(_ input: Number) -> StructuredData {
+
+    switch input {
+    case .integer(let i):
+      return .int(Int(i))
+    case .double(let d):
+      return .double(d)
+    }
+  }
+
+  typealias Number = GenericJsonParser.Number
+
+  public static func parse(_ data: Data) throws -> StructuredData {
+
+    var bytes = data.bytes
+
+    return try GenericJsonParser.parse(
+      data: &bytes,
+      onObject: toObject,
+      onArray: toArray,
+      onNull: toNull,
+      onBool: toBool,
+      onString: toString,
+      onNumber: toNumber
+    )
+  }
+}
+
